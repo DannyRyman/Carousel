6bf1dfd2c8b749434f55f09f994c095f7fc2b100
dan ryan <danielr@digiterre.com>
2018-10-20 08:25:27 +0100

    Tidy App Component
    1. Remove the existing logo
    2. Change the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import * as React from 'react'
 import './App.css'
 
-import logo from './logo.svg'
-
 class App extends React.Component {
   public render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
+      <div className='app'>
+        <header className='app__header'>
+          <h1>Carousel Test</h1>
         </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+        <p>
+          Todo Carousel
         </p>
       </div>
     )
